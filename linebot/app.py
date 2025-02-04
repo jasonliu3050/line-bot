db6db60eccc661e1321b531bc0a8482b63a60e15
jasonliu3050 <jasonliu3050@gmail.com>
2025-02-04 09:48:20 +0800

    Update app.py

--- linebot/app.py
+++ linebot/app.py
@@ -60,10 +60,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
-
-
-if __name__ == "__main__":
-    # 取得 Render 提供的 PORT，若無則預設 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
