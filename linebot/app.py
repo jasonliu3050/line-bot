5f28f3429ecebb42ddb9d8be0f1ae4136898cfd2
jasonliu3050 <jasonliu3050@gmail.com>
2025-02-04 11:21:43 +0800

    Update app.py

--- linebot/app.py
+++ linebot/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
-from linebot.v3.messaging import MessagingApi, TextMessage, TextSendMessage
+from linebot.v3.messaging import MessagingApi
+from linebot.v3.messaging.models import TextMessage, TextSendMessage
 from linebot.v3.webhook import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.webhooks import MessageEvent, PostbackEvent
